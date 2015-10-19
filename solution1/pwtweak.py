0925cfe51b216f28222463b2b8b0de7171d9c344
tsenggordon <kuoweitseng@gmail.com>
2015-10-19 18:56:07 -0400

    simple tweak

--- solution1/pwtweak.py
+++ solution1/pwtweak.py
@@ -1,16 +1,20 @@
 import string
 import random
+import sys
+import csv
 
 def gen(pw, k, t):
-	char = 1
-	no = 2
-	special = 3
-	special_ex = '[~!@#$%^&*()_+{}":;\\\']+$'
-	if t > len(pw):
-		t = len(pw)
-	honey_list = []
-	for k in range(0,k):
-		hoeny_tail = []
+    char = 1
+    no = 2
+    special = 3
+    special_ex = '[~!@#$%^&*()_+{}":;\\\']+$'
+    if t > len(pw):
+        t = len(pw)
+        
+	
+    honey_list = []
+    for k in range(0,k):
+		honey_tail = []
 		for c in pw[-t:]:
 			if c.isdigit():
 				honey_tail.append(random.choice(string.digits) )
@@ -19,7 +23,7 @@ def gen(pw, k, t):
 			else:
 				honey_tail.append(random.choice(string.ascii_letters) )
 		honey_list.append(pw[:len(pw)- t]+''.join(honey_tail))
-	print honey_list
+    return honey_list
 		
 
 def gen_digits(pw, k, t):
@@ -40,4 +44,29 @@ def pwSeperate(s):
 			start = idx+1
 	#print pw_seg
 	return pw_seg, pw_seg_isdigit	
-		
+def read_input_file(filename):
+    with open(filename) as f:
+        return [i.strip() for i in f.readlines() if len(i.strip()) > 0]
+def write_csv(filename, values):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(values)
+def password_gen(input_f, n):
+	pw_list = read_input_file(input_f)
+	return [gen(pw, n, 3) for pw in pw_list]
+
+def main(argv):
+    if len(sys.argv) != 4:
+        print "Generate the honeywords."
+        print ""
+        print "python solution3.py n inputfile outputfile"
+        print ""
+        print "     n               the number of passwords"
+        print "     inputfile       the filename of inputfile"
+        print "     outputfile      the filename of outputfile"
+        sys.exit(1)
+    sweetwords = password_gen(argv[2], int(argv[1]))
+    write_csv(argv[3], sweetwords)
+
+if __name__ == "__main__":
+    main(sys.argv)
